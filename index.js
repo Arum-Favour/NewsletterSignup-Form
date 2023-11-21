ee44309995f13fd45eb968060d9d469532eebeba
Kingson.Arum <nwajiudo@gmail.com>
2023-11-21 21:28:58 +0100

    commit

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var successMsg = document.querySelector(".success-msg");
 
 
 function myFunction(){
-    // var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    // if(emailInput){
-    //     signupPage.style.visibility="hidden";
-    //     successMsg.style.visibility="visible";
-    // }
-    alert("A Button was clicked");
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(emailInput){
+        signupPage.style.visibility="hidden";
+        successMsg.style.visibility="visible";
+    }
+    // alert("A Button was clicked");
 }
