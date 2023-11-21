0b4e516070cfa429120347413d95763c59463476
Kingson.Arum <nwajiudo@gmail.com>
2023-11-21 23:48:08 +0100

    commit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function myFunction() {
   const signupPage = document.getElementById("signup-page");
   const successMsg = document.querySelector(".success-msg");
   const emailValue = document.querySelector("strong");
+  const errorColor ="#FFCCCB";
+  const errorMsg = document.querySelector("#errorMsg");
   var validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (emailInput.value.match(validRegex)) {
@@ -10,8 +12,11 @@ function myFunction() {
     signupPage.style.visibility = "hidden";
     successMsg.style.visibility = "visible";
     emailValue.innerHTML= emailInput.value;
+    errorMsg.style.visibility = "visible";
   } else {
     emailInput.style.borderColor = "red";
+    emailInput.style.color = "red";
+    emailInput.style.backgroundColor = errorColor;
   }
 }
 
