e280ecdd11b4cd46e8b47797d381879e64ee1c9d
Kingson.Arum <nwajiudo@gmail.com>
2023-11-21 18:10:28 +0100

    commit

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-console.log("hello Yall");
+var emailInput = document.getElementById("email-input");
+var signupPage = document.querySelector("#signup-page");
+var successMsg = document.querySelector(".success-msg");
 
 
-
-
-function emailValidator(input){
-
+function myFunction(){
+    // var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    // if(emailInput){
+    //     signupPage.style.visibility="hidden";
+    //     successMsg.style.visibility="visible";
+    // }
+    alert("A Button was clicked");
 }
