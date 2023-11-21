264e6d5f02272de8bc715e34125cb99bce29531b
Kingson.Arum <nwajiudo@gmail.com>
2023-11-21 21:36:48 +0100

    commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var emailInput = document.getElementById("email-input");
-var signupPage = document.querySelector("#signup-page");
-var successMsg = document.querySelector(".success-msg");
+function myFunction() {
+  var emailInput = document.getElementById("email-input");
+  var signupPage = document.getElementById("signup-page");
+  var successMsg = document.getElementsByClassName("success-msg");
 
-
-function myFunction(){
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(emailInput){
-        signupPage.style.visibility="hidden";
-        successMsg.style.visibility="visible";
-    }
-    // alert("A Button was clicked");
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (emailInput) {
+    signupPage.style.visibility = "hidden";
+    successMsg.style.display = "flex";
+    successMsg.style.visibility = "visible";
+  }
+  // alert("A Button was clicked");
 }
