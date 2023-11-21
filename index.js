ae54eeaa307ec3375b10523a7129791017b73221
Kingson.Arum <nwajiudo@gmail.com>
2023-11-21 22:23:21 +0100

    commit

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const emailInput = document.getElementById("email-input");
-const signupPage = document.getElementById("signup-page");
-const successMsg = document.querySelector(".success-msg");
-
 function myFunction() {
+  const emailInput = document.getElementById("email-input");
+  const signupPage = document.getElementById("signup-page");
+  const successMsg = document.querySelector(".success-msg");
+  const emailValue = document.querySelector("strong");
   var validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (emailInput.value.match(validRegex)) {
     successMsg.style.display = "flex";
     signupPage.style.visibility = "hidden";
     successMsg.style.visibility = "visible";
+    emailValue.innerHTML= emailInput.value;
   } else {
     emailInput.style.borderColor = "red";
   }
 }
 
 function dismissContent() {
-  successMsg.style.display = "none";
-  signupPage.style.visibility = "visible";
-  successMsg.style.visibility = "hidden";
+    window.location.reload("Refresh");
 }
